b28ec37376af0057521616d4e1bc27e1e6f266d8
Sonu Kurian <sonu.kurian@in.ibm.com>
2021-04-14 14:43:56 +0530

    first commit

---
+++ src/test/java/com/sparktech/microservices/sparktechspringcloudconfigserver/SparktechSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparktech.microservices.sparktechspringcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparktechSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
